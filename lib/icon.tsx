2baa1b4344861bc13e12c8c63dfbe45fb00f9501
susonglin <susonglin@deepexi.com>
2020-10-12 22:08:28 +0800

    feat: ico支持事件

--- lib/icon.tsx
+++ lib/icon.tsx
@@ -2,14 +2,16 @@ import React from "react";
 import "./importIcon.js"; //导入所有icon
 import "./icon.scss"
 
-interface IconProps {
-  name:string
+// IconProps 继承SVGAttributes 接收SVGAElement或HTMLElement
+interface IconProps extends React.SVGAttributes<SVGElement> {
+  name:string,
+  onClick:React.MouseEventHandler<SVGElement>
 }
 
 // 声明icon 为函数Component 参数为IconProps
 const Icon:React.FunctionComponent<IconProps> = (props)=>{
   return(
-    <svg  className="icon">
+    <svg  className="icon" {...props}>
       <use xlinkHref={`#${props.name}`}></use>
     </svg>
   )
