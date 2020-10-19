8289b4ad0696ae03182e314e997a7bea90cd8414
susonglin <susonglin@deepexi.com>
2020-10-19 22:42:12 +0800

    fix: 移动icon目录

--- lib/icon/icon.tsx
+++ lib/icon/icon.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./importIcon.js"; //导入所有icon
 import "./icon.scss"
-import classes from "./helpers/classes";
+import classes from "../helpers/classes";
 
 // IconProps 继承SVGAttributes 接收SVGAElement或HTMLElement
 interface IconProps extends React.SVGAttributes<SVGElement> {
--- lib/index.tsx
+++ lib/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Icon from './icon'
+import Icon from './icon/icon'
 
 ReactDOM.render(
   <Icon name="wechat"  onClick={()=>{console.log(1)}}></Icon>
