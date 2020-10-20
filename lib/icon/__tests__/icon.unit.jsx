5835f393b97e22c6ac141ce7d2dff6c5f873a0bb
susonglin <susonglin@deepexi.com>
2020-10-20 21:51:11 +0800

    test: 修改icon onClick测试方式

--- lib/icon/__tests__/icon.unit.jsx
+++ lib/icon/__tests__/icon.unit.jsx
@@ -9,13 +9,11 @@ describe('Icon', () => {
         expect(json).toMatchSnapshot()
     })
     it('onClick点击事件', () => {
-        let n = 1
-        const fn = ()=>{
-            n = 333
-        }
+
+        const fn = jest.fn()
         const c = mount(<Icon name="alipay" onClick={fn} />)
-        c.find('svg').simulate('click') // 如果点击了那么n 就等于333
-        expect(n).toEqual(333)
+        c.find('svg').simulate('click') 
+        expect(fn).toBeCalled() //期待fn被调用
 
     })
 })
