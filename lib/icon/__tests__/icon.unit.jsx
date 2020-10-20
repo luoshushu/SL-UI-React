1a213c71fb94c5e1d81998fc3f574c4211ece359
susonglin <susonglin@deepexi.com>
2020-10-20 21:49:11 +0800

    test: icon 单击事件测试

--- lib/icon/__tests__/icon.unit.jsx
+++ lib/icon/__tests__/icon.unit.jsx
@@ -1,10 +1,21 @@
 import * as renderer from 'react-test-renderer'
 import React from 'react'
 import Icon from '../icon'
+import { mount } from "enzyme";
 
 describe('Icon', () => {
-    it('是个 svg', () => {
-        const json = renderer.create(<Icon/>).toJSON()
+    it('是个 svg', () => { 
+        const json = renderer.create(<Icon name="alipay"/>).toJSON()
         expect(json).toMatchSnapshot()
+    })
+    it('onClick点击事件', () => {
+        let n = 1
+        const fn = ()=>{
+            n = 333
+        }
+        const c = mount(<Icon name="alipay" onClick={fn} />)
+        c.find('svg').simulate('click') // 如果点击了那么n 就等于333
+        expect(n).toEqual(333)
+
     })
 })
---
+++ test/README.md
@@ -0,0 +1,4 @@
+```
+yarn test -u //更新快照
+yarn test --watch // 监听测试文件变化 热更新
+```
