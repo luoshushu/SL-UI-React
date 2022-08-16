6380d747c9adab9f26f20992414c93c2b8d304bb
susonglin <susonglin@cmcm.com>
2022-08-16 22:26:31 +0800

    feat: 更新 index.tsx 导出 Icon 组件

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,7 +1,1 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Icon from './icon/icon'
-
-ReactDOM.render(
-  <Icon name="wechat"  onClick={()=>{console.log(1)}}></Icon>
-  ,document.getElementById('root'))
+export { default as Icon } from './icon/icon'
